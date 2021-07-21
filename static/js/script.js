f0718ed3e4c4c63ac9fcd914d5c12ff921ec7caf
JoseMachado-CE <jose@letsgetchecked.com>
2021-07-21 23:23:54 +0000

    Updated register file

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,15 @@
+import swal from 'sweetalert';
+
+
 $(document).ready(function() {
     $('.sidenav').sidenav();
 })
+
+function displayError(message) {
+    swal.fire({
+        title: 'Error!',
+        text: `${message}`,
+        icon: 'error',
+        confirmButtonText: 'Ok',
+    });
+}
