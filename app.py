8cd0ac2030e71f90a2a62e185f6df1f4383ff786
JoseMachado-CE <jose@letsgetchecked.com>
2021-08-25 20:59:19 +0000

    Updated env file adding debug env variable

--- app.py
+++ app.py
@@ -188,5 +188,6 @@ def search():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=os.environ.get('DBUG'))
+            
             
